dc3b79b7ee008aa93ae7012bc6405ca2df747057
Paul Wisdom <itmm103samsonpaulwisdom@gmail.com>
2018-06-30 22:36:06 +0800

    First attempt on server

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Log in Page</title>
+</head>
+<body>
+
+
+<div>
+	<form action = "process.php" method = "POST">
+		<p>
+			<label>Username</label>
+			<input type="text" id ="user" name="user" />
+		</p>
+
+			<p>
+			<label>Password</label>
+			<input type="password" id ="pass" name="pass" />
+		</p>
+
+		<p>
+			<input type="submit" id ="btn" name="Login" />
+		</p>
+
+	</form>
+</div>
+
+</body>
+</html>
